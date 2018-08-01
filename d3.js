53abb46616765363d4e7bdf927dbe1e1b2ab0c99
efernandez <efernandez>
2018-08-01 12:11:39 +0200

    Added transition

--- d3.js
+++ d3.js
@@ -2,10 +2,10 @@ import { line } from "d3-shape";
 import { scaleTime, scaleLinear } from "d3-scale";
 import { axisBottom, axisLeft } from 'd3-axis';
 import { timeParse } from "d3-time-format";
-import { select } from "d3-selection";
+import { select, Selection } from "d3-selection";
 import { csv } from "d3-request";
 import { extent } from "d3-array";
-import { transition } from "d3-transition";
+import { transition, Transition } from "d3-transition";
 
 export default {
   line: line,
